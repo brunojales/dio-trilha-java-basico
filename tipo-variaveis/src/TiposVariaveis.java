09e9abfbe6dcfed624d0ad6b4dc6a370b4897efd
Bruno <bruno.jales.90@gmail.com>
2024-05-24 01:06:24 -0300

    TipoPrimitivos-Variaveis

--- tipo-variaveis/src/TiposVariaveis.java
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,25 @@
-public class App {
+
+public class TiposVariaveis {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        //Tipos primitivos
+
+        //Exemplo de VARIAVEIS
+        double salarioMinimo = 1400.75;
+        //Não seu usar virgula para serparar milhar, mas sim centavos(padrão Americano)
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+
+        //Exemplo de CONSTANTES 
+        //precisa ter nomenclatura 'final' e o nome da constante deve estar MAIUSCULO
+
+        final double VALOR_DE_PI = 3.14;
+
+    
+    
     }
 }
