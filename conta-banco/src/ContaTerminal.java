747886ba4476b1dc3cda2fef38b943b4b296afc3
bruno jales <bruno.jales.90@gmail.com>
2024-08-08 16:07:36 -0300

    Implementação do codigo com a classe Scanner

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+   
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Informe a sua Agência: ");
+    String agencia = scanner.nextLine();
+
+    System.out.println("Informe o numero da sua Conta: ");
+    int numero = scanner.nextInt();
+
+    scanner. nextLine();
+
+    System.out.println("Informe o seu nome completo: ");
+    String nomeCliente = scanner.nextLine();
+
+    System.out.println("Por favor, informar seu Saldo: ");
+    double saldo = scanner.nextDouble();
+
+
+
+    System.out.println ("Olá " +nomeCliente+ ", obrigado por criar uma conta em nosso banco, sua Agência é " +agencia+ " e seu saldo de R$" +saldo+ " já está disponivel para saque." );
+
+        scanner.close();
+
     }
 }
